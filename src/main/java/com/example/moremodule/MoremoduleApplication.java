d62394f556aab70126f514d49eacdd36bc355fd0
home <zeng92922@163.com>
2020-10-20 21:24:01 +0800

    Initial commit

---
+++ src/main/java/com/example/moremodule/MoremoduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.moremodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoremoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoremoduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moremodule/MoremoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moremodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoremoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
