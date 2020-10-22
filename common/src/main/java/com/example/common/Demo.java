c4d43f98ceabbe5488b510eab7fe97f26c5958d0
zengguanghao <zeng92922@163.com>
2020-10-22 20:36:40 +0800

    多模块示例（1）

---
+++ common/src/main/java/com/example/common/Demo.java
@@ -0,0 +1,10 @@
+package com.example.common;
+
+public class Demo {
+    public void Ceshi(){
+        System.out.println("aaa");
+    }
+    public void Demo(){
+        System.out.println("bbb");
+    }
+}
---
+++ test/src/main/java/com/example/test/Main.java
@@ -0,0 +1,12 @@
+package com.example.test;
+
+import com.example.common.Demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.Ceshi();
+        demo.Demo();
+    }
+}
