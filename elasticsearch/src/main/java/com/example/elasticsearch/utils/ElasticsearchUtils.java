4766363074a004e6ed15a0b3057d5b61b106a832
zengguanghao <zengguanghao@bonc.com.cn>
2020-12-16 11:07:51 +0800

    es使用match 查询某些特定字段的方法

--- elasticsearch/src/main/java/com/example/elasticsearch/utils/ElasticsearchUtils.java
+++ elasticsearch/src/main/java/com/example/elasticsearch/utils/ElasticsearchUtils.java
@@ -7,6 +7,8 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.*;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -55,7 +57,7 @@ public class ElasticsearchUtils {
 
         for (SearchHit hit : hits.getHits()) {
             Map<String, HighlightField> highlightFields = hit.getHighlightFields();
-            if (highlightFields.size() != 0){
+            if (highlightFields.size() != 0) {
                 System.out.println("bbb");
             }
         }
@@ -63,9 +65,45 @@ public class ElasticsearchUtils {
         System.out.println("aaa");
     }
 
+    /**
+     * 根据某个字段进行查询   返回mapping中的某些字段
+     * @param index
+     * @param field
+     * @param result
+     * @throws IOException
+     */
+    public static void searchByfield(String index, String field, String result) throws IOException {
+        initHighClient();
+        SearchRequest request = new SearchRequest(index);
+        request.searchType(SearchType.DEFAULT);
+
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        MatchQueryBuilder query = QueryBuilders.matchQuery(field, result);
+        builder.query(query);
+        request.source(builder);
+        request.source(builder);
+
+        SearchResponse response = higeClient.search(request, RequestOptions.DEFAULT);
+        SearchHits hits = response.getHits();
+
+        long totalHits = hits.getTotalHits();
+        System.out.println("total hit is " + totalHits);
+
+        for (SearchHit hit : hits.getHits()) {
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            System.out.println(sourceAsMap.get("info"));
+            System.out.println(sourceAsMap.get("class"));
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+
 //        source("customer","_doc","5");
 //        higeClient.close();
-        search("customer", 0, 10);
+//        search("customer", 0, 10);
+        searchByfield("springboot","class","ERROR");
     }
+
+
 }
