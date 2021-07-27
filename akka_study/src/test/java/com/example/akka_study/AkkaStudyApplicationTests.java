706d1e5c07aeffc65f30d21c65540005fc2f5b6e
zengguanghao <zengguanghao@bonc.com.cn>
2021-07-27 16:24:40 +0800

    新建akka 学习框架

---
+++ akka_study/src/test/java/com/example/akka_study/AkkaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.akka_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkkaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
