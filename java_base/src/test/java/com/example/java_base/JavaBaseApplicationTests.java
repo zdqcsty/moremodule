cc36a049042b9a27bec5049a6d3ceb5bebfb1d36
zengguanghao <zengguanghao@bonc.com.cn>
2021-08-02 15:36:17 +0800

    新建项目java_base  用于写java相关的demo

---
+++ java_base/src/test/java/com/example/java_base/JavaBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
